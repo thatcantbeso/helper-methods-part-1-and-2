8c7578f63f18d16acd0b1e5640c7790ce6ed1059
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-10-16 21:10:32 +0000

    controller trimming - renders

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,9 +15,7 @@ class MoviesController < ApplicationController
         render json: @list_of_movies
       end
 
-      format.html do
-        render template: "movies/index"
-      end
+      format.html
     end
   end
 
@@ -27,8 +25,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render template: "movies/show"
   end
 
   def create
@@ -40,7 +36,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to movies_url, notice: "Movie created successfully."
     else
-      render template: "movies/new"
+      render template: "new"
     end
   end
 
@@ -50,8 +46,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render template: "movies/edit"
   end
 
   def update
