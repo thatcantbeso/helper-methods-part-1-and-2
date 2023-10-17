aee66d936b99eb6f64c3c7c0a0a9edfa72b6831d
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-10-17 16:40:39 +0000

    new refactoring

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -36,7 +36,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to movies_url, notice: "Movie created successfully."
     else
-      render template: "new"
+      render "new"
     end
   end
 
