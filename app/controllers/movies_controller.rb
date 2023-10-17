68ec89bc8038e9ad3c61d1aea126cdebd8214efc
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-10-17 20:59:34 +0000

    started subhash

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -25,8 +25,8 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new
-    @movie.title = params.fetch("title")
-    @movie.description = params.fetch("description")
+    @movie.title = params.fetch(:movie).fetch(:title )
+    @movie.description = params.fetch(:movie).fetch(:description)
 
     if @movie.valid?
       @movie.save
@@ -43,8 +43,8 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params.fetch(:id))
 
-    movie.title = params.fetch("title")
-    movie.description = params.fetch("description")
+    movie.title = params.fetch(:title)
+    movie.description = params.fetch(:description)
 
     if movie.valid?
       movie.save
